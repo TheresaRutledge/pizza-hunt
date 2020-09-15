78f3664d00835604fddee6d298aa58da3c5d93eb
Theresa Rutledge <t.roserutledge@gmail.com>
2020-09-15 11:25:04 -0700

    comments model & route, added moment

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,45 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-      type: String
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYY [at] hh:mm a')
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
     },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: []
-  });
+    id: false
+  }
+);
 
-  const Pizza = model('Pizza', PizzaSchema);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
 
-  module.exports = Pizza;
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
